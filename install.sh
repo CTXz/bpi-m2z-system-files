76912027052af3dea3615920e0316c6c82305cbe
Patrick Pedersen <ctx.xda@gmail.com>
2023-01-16 17:38:59 +0100

    install.sh: Fixed folders getting copied into folders

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-cp -v -r $SCRIPT_DIR/etc /etc
+cp -v -r $SCRIPT_DIR/etc/* /etc/
 
 if [ $? -ne 0 ]
   then echo "Failed to copy $SCRIPT_DIR/etc to /etc"
   exit 1
 fi
 
-cp -v -r $SCRIPT_DIR/usr /usr
+cp -v -r $SCRIPT_DIR/usr/* /usr/
 
 if [ $? -ne 0 ]
   then echo "Failed to copy $SCRIPT_DIR/usr to /usr"
   exit 1
 fi
 
-cp -v -r $SCRIPT_DIR/var /var
+cp -v -r $SCRIPT_DIR/var/* /var/
 
 if [ $? -ne 0 ]
   then echo "Failed to copy $SCRIPT_DIR/var to /var"
