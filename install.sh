da80a6f46ee142e4882ee6aa25cdf401031e21bb
Patrick Pedersen <ctx.xda@gmail.com>
2023-01-16 17:23:14 +0100

    install.sh: Added error checks

--- install.sh
+++ install.sh
@@ -8,7 +8,24 @@ if [ "$EUID" -ne 0 ]
 fi
 
 cp -v -r $SCRIPT_DIR/etc /etc
+
+if [ $? -ne 0 ]
+  then echo "Failed to copy $SCRIPT_DIR/etc to /etc"
+  exit 1
+fi
+
 cp -v -r $SCRIPT_DIR/usr /usr
+
+if [ $? -ne 0 ]
+  then echo "Failed to copy $SCRIPT_DIR/usr to /usr"
+  exit 1
+fi
+
 cp -v -r $SCRIPT_DIR/var /var
+
+if [ $? -ne 0 ]
+  then echo "Failed to copy $SCRIPT_DIR/var to /var"
+  exit 1
+fi
 
 echo "System files installed!"
